0b05285d0e9ae951db2a89d027b1edfa03ebe610
kana <chocolate.bar.favorite@gmail.com>
2024-03-24 13:21:45 +0900

    made dog3

--- src/main/java/kana/project/Main.java
+++ src/main/java/kana/project/Main.java
@@ -5,8 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Dog dog1 = new Dog("Cisco", 10, "F", true);
         Dog dog2 = new Dog("Kana", 31, "M", false);
+        Dog dog3 = new Dog("Poch", 30, "F", false);
+
         System.out.println(dog2.getAge());
         dog1.bow();
         dog2.bow();
+        dog3.bow();
     }
 }
